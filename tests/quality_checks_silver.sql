5b0d6807b79a540d98a82e2b306eb7b75423a61e
Thanaphut Aksornprasert <117530788+FlowerPot30@users.noreply.github.com>
2026-01-26 14:43:37 +0700

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,18 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+  This script performs various quality checks for data consistency, accuracy,
+  and standardization across the 'silver' layer. It includes checks for:
+  - Null or duplicate primary keys.
+  - Unwanted spaces in string fields.
+  - Data standardization and consistency.
+  - Data consistency between related fields.
+
+Usage Notes:
+  - Run these checks after data loading Silver Layer.
+  - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
 
